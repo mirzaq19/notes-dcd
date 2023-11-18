3977f9bf929c9395e90a5df9776d3324dfa171f8
M. Auliya Mirzaq Romdloni <mirzaqarjap@gmail.com>
2023-11-18 14:40:37 +0700

    refactor: put theme functionality to other file

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,14 @@
-import { useEffect, useState } from 'react'
 import Layout from './components/Layout'
-import { ThemeProvider } from './contexts/theme'
 import NotesForm from './components/NotesForm'
+import ThemeWrapper from './components/ThemeWrapper'
 
 function App() {
-  const userPrefersDark =
-    window.matchMedia &&
-    window.matchMedia('(prefers-color-scheme: dark)').matches
-
-  const localStorageTheme = localStorage.getItem('theme')
-  const [themeMode, setThemeMode] = useState(
-    localStorageTheme ? localStorageTheme : userPrefersDark ? 'dark' : 'light'
-  )
-
-  const lightTheme = () => {
-    setThemeMode('light')
-    localStorage.setItem('theme', 'light')
-  }
-
-  const darkTheme = () => {
-    setThemeMode('dark')
-    localStorage.setItem('theme', 'dark')
-  }
-
-  useEffect(() => {
-    document.querySelector('body').setAttribute('data-theme', themeMode)
-  }, [themeMode])
-
   return (
-    <ThemeProvider value={{ themeMode, lightTheme, darkTheme }}>
+    <ThemeWrapper>
       <Layout>
         <NotesForm />
       </Layout>
-    </ThemeProvider>
+    </ThemeWrapper>
   )
 }
 
---
+++ src/components/ThemeWrapper.jsx
@@ -0,0 +1,33 @@
+import { useEffect, useState } from 'react'
+import { ThemeProvider } from '../contexts/theme'
+const ThemeWrapper = ({ children }) => {
+  const userPrefersDark =
+    window.matchMedia &&
+    window.matchMedia('(prefers-color-scheme: dark)').matches
+
+  const localStorageTheme = localStorage.getItem('theme')
+  const [themeMode, setThemeMode] = useState(
+    localStorageTheme ? localStorageTheme : userPrefersDark ? 'dark' : 'light'
+  )
+
+  const lightTheme = () => {
+    setThemeMode('light')
+    localStorage.setItem('theme', 'light')
+  }
+
+  const darkTheme = () => {
+    setThemeMode('dark')
+    localStorage.setItem('theme', 'dark')
+  }
+
+  useEffect(() => {
+    document.querySelector('body').setAttribute('data-theme', themeMode)
+  }, [themeMode])
+  return (
+    <ThemeProvider value={{ themeMode, lightTheme, darkTheme }}>
+      {children}
+    </ThemeProvider>
+  )
+}
+
+export default ThemeWrapper
