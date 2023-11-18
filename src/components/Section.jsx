add19230e0dc6f355d3e7828f4b5a4b0228e1529
M. Auliya Mirzaq Romdloni <mirzaqarjap@gmail.com>
2023-11-18 13:57:56 +0700

    feat: add section component

---
+++ src/components/Section.jsx
@@ -0,0 +1,13 @@
+import styles from './Section.module.scss'
+
+const Section = ({ title, children, divider = true }) => {
+  return (
+    <section className={styles.container}>
+      <h2 className={styles.title}>{title}</h2>
+      {children}
+      {divider && <hr className={styles.divider} />}
+    </section>
+  )
+}
+
+export default Section
