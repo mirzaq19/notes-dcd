94240f6a2eab9fed0fd82ab047a21e27b9ec2930
M. Auliya Mirzaq Romdloni <mirzaqarjap@gmail.com>
2023-11-18 10:37:04 +0700

    feat: add input with counter component

---
+++ src/components/InputWithCounter.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+import styles from './Input.module.scss'
+
+const Input = ({ required, placeholder, value, setValue, maxCounter = 50 }) => {
+  const [counter, setCounter] = useState(maxCounter)
+
+  const onChangeInput = (e) => {
+    const value = e.target.value
+
+    if (value.length <= maxCounter) {
+      setCounter(maxCounter - value.length)
+      setValue(value)
+    }
+  }
+
+  return (
+    <>
+      <div className={styles.counter}>
+        <span>Sisa karakter: {counter}</span>
+      </div>
+      <input
+        required={required}
+        onChange={onChangeInput}
+        value={value}
+        className={styles.input}
+        type="text"
+        placeholder={placeholder}
+      />
+    </>
+  )
+}
+
+export default Input
