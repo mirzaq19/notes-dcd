900b359847e35c0f37275255076bf40cfd3260ef
M. Auliya Mirzaq Romdloni <mirzaqarjap@gmail.com>
2023-12-15 13:45:49 +0700

    refactor: add props validation

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,17 +1,24 @@
 import Input from '@/components/form/Input'
 import Section from '@/components/layout/Section'
+import PropTypes from 'prop-types'
+import clsx from 'clsx'
 
-const Search = ({ value, setValue }) => {
+const Search = ({ className, value, setValue, ...rest }) => {
   return (
-    <Section title="Cari Catatan">
+    <Section title="Cari Catatan" className={clsx(className)} {...rest}>
       <Input
-        required={false}
         placeholder="Tuliskan judul catatan untuk mencari"
         value={value}
         setValue={setValue}
       />
     </Section>
   )
+}
+
+Search.propTypes = {
+  className: PropTypes.string,
+  value: PropTypes.string.isRequired,
+  setValue: PropTypes.func.isRequired,
 }
 
 export default Search
