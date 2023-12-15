bf8367216ac05e37f465bdb1530f832bf2afd93b
M. Auliya Mirzaq Romdloni <mirzaqarjap@gmail.com>
2023-12-15 12:31:30 +0700

    feat: add custom color in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,18 @@ import { fontFamily } from 'tailwindcss/defaultTheme'
 
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  darkMode: ['class', '[data-theme="dark"]'],
   theme: {
     extend: {
+      colors: {
+        dark: 'rgb(var(--tw-clr-dark) / <alpha-value>)',
+        light: 'rgb(var(--tw-clr-light) / <alpha-value>)',
+        secondary: 'rgb(var(--tw-clr-secondary) / <alpha-value>)',
+        accent: {
+          peach: 'rgb(var(--tw-clr-accent-peach) / <alpha-value>)',
+          grape: 'rgb(var(--tw-clr-accent-grape) / <alpha-value>)',
+        },
+      },
       fontFamily: {
         sans: ['Inter', ...fontFamily.sans],
       },
