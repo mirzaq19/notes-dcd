b8cfedb89a66b526f5d2cfe02f7629bfa6f0e810
M. Auliya Mirzaq Romdloni <mirzaqarjap@gmail.com>
2023-12-15 15:54:17 +0700

    feat: add home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
 import { Routes, Route } from 'react-router-dom'
 import Home from '@/pages/Home'
 import Navbar from '@/components/layout/Navbar'
+import Container from '@/components/layout/Container'
+import Footer from '@/components/layout/Footer'
 
 function App() {
   return (
     <>
       <Navbar />
       <main>
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
+        <Container>
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+          <Footer />
+        </Container>
       </main>
     </>
   )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,25 @@
+import NoteList from '@/components/NoteList'
+import Search from '@/components/Search'
+import Section from '@/components/layout/Section'
+import { getActiveNotes } from '@/utilities/data'
+import { useState } from 'react'
+
 const Home = () => {
-  return <div>Home</div>
+  const [keyword, setKeyword] = useState('')
+  const [notes] = useState(getActiveNotes())
+
+  const notesToRender = notes.filter((note) =>
+    note.title.toLowerCase().includes(keyword.toLowerCase())
+  )
+
+  return (
+    <div className="min-h-main">
+      <Search value={keyword} setValue={(value) => setKeyword(value)} />
+      <Section title="Catatan Aktif">
+        <NoteList notes={notesToRender} />
+      </Section>
+    </div>
+  )
 }
 
 export default Home
