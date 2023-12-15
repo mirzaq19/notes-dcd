c653d6c8dc7e22cca5c008fb464bf638a6ba4df2
M. Auliya Mirzaq Romdloni <mirzaqarjap@gmail.com>
2023-12-15 07:39:08 +0700

    chore: install tailwindcss and using react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,63 +1,82 @@
-import NoteList from './components/NoteList'
-import Layout from './components/Layout'
-import NotesForm from './components/NotesForm'
-import Search from './components/Search'
-import ThemeWrapper from './components/ThemeWrapper'
-import { getInitialData } from './utilities/getData'
-import { useEffect, useState } from 'react'
+// import NoteList from './components/NoteList'
+// import Layout from './components/Layout'
+// import NotesForm from './components/NotesForm'
+// import Search from './components/Search'
+// import ThemeWrapper from './components/ThemeWrapper'
+// import { getInitialData } from './utilities/getData'
+// import { useEffect, useState } from 'react'
+
+// function App() {
+//   const [notes, setNotes] = useState(getInitialData)
+//   const [searchNotes, setSearchNotes] = useState([])
+//   const [keyword, setKeyword] = useState('')
+
+//   const onArchiveHandler = (id) => {
+//     const newNotes = notes.map((note) => {
+//       if (note.id === id) {
+//         return { ...note, archived: !note.archived }
+//       }
+//       return note
+//     })
+//     setNotes(newNotes)
+//   }
+
+//   const onDeleteHandler = (id) => {
+//     const newNotes = notes.filter((note) => note.id !== id)
+//     setNotes(newNotes)
+//   }
+
+//   const onAddNoteHandler = (note) => {
+//     setNotes([
+//       ...notes,
+//       {
+//         ...note,
+//         id: +new Date(),
+//         archived: false,
+//         createdAt: new Date().toISOString(),
+//       },
+//     ])
+//   }
+
+//   useEffect(() => {
+//     const newNotes = notes.filter((note) => {
+//       const title = note.title.toLowerCase()
+//       return title.includes(keyword.toLowerCase())
+//     })
+//     setSearchNotes(newNotes)
+//   }, [keyword, notes])
+
+//   return (
+//     <ThemeWrapper>
+//       <Layout>
+//         <NotesForm onAddNote={onAddNoteHandler} />
+//         <Search value={keyword} setValue={(value) => setKeyword(value)} />
+//         <NoteList
+//           notes={searchNotes}
+//           onArchive={onArchiveHandler}
+//           onDelete={onDeleteHandler}
+//         />
+//       </Layout>
+//     </ThemeWrapper>
+//   )
+// }
+
+// export default App
+import { Routes, Route } from 'react-router-dom'
+import Home from '@/pages/Home'
 
 function App() {
-  const [notes, setNotes] = useState(getInitialData)
-  const [searchNotes, setSearchNotes] = useState([])
-  const [keyword, setKeyword] = useState('')
-
-  const onArchiveHandler = (id) => {
-    const newNotes = notes.map((note) => {
-      if (note.id === id) {
-        return { ...note, archived: !note.archived }
-      }
-      return note
-    })
-    setNotes(newNotes)
-  }
-
-  const onDeleteHandler = (id) => {
-    const newNotes = notes.filter((note) => note.id !== id)
-    setNotes(newNotes)
-  }
-
-  const onAddNoteHandler = (note) => {
-    setNotes([
-      ...notes,
-      {
-        ...note,
-        id: +new Date(),
-        archived: false,
-        createdAt: new Date().toISOString(),
-      },
-    ])
-  }
-
-  useEffect(() => {
-    const newNotes = notes.filter((note) => {
-      const title = note.title.toLowerCase()
-      return title.includes(keyword.toLowerCase())
-    })
-    setSearchNotes(newNotes)
-  }, [keyword, notes])
-
   return (
-    <ThemeWrapper>
-      <Layout>
-        <NotesForm onAddNote={onAddNoteHandler} />
-        <Search value={keyword} setValue={(value) => setKeyword(value)} />
-        <NoteList
-          notes={searchNotes}
-          onArchive={onArchiveHandler}
-          onDelete={onDeleteHandler}
-        />
-      </Layout>
-    </ThemeWrapper>
+    <>
+      <header>
+        <h1>NotesApp</h1>
+      </header>
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </main>
+    </>
   )
 }
 
