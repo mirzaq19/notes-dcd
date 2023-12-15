7fe752138eb16d05434dd6fdfab5d0b738de8b1d
M. Auliya Mirzaq Romdloni <mirzaqarjap@gmail.com>
2023-12-15 22:05:23 +0700

    refactor: add prop validation in button component

--- src/components/buttons/Button.jsx
+++ src/components/buttons/Button.jsx
@@ -1,12 +1,25 @@
-import styles from '@/styles/Button.module.scss'
-const Button = ({ full, onClick, children }) => {
+import clsx from 'clsx'
+import PropTypes from 'prop-types'
+
+const Button = ({ className, children, ...rest }) => {
   return (
     <button
-      onClick={onClick}
-      className={`${styles.button} ${full ? styles.fullbutton : ''}`}
+      className={clsx(
+        'flex items-center justify-center gap-2 px-4 py-2 rounded-md',
+        'bg-gradient-to-r from-accent-peach to-accent-grape text-white',
+        'transition duration-300 ease-in-out',
+        'hover:shadow-accent active:scale-[0.96]',
+        className
+      )}
+      {...rest}
     >
       {children}
     </button>
   )
+}
+
+Button.propTypes = {
+  className: PropTypes.string,
+  children: PropTypes.node.isRequired,
 }
 export default Button
