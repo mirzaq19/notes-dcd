1981bcd1558d96b0c0d16f9fb6ca6ee9e1ce3262
M. Auliya Mirzaq Romdloni <mirzaqarjap@gmail.com>
2023-12-15 16:35:06 +0700

    feat: sync search bar with query params

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,18 +3,24 @@ import Search from '@/components/Search'
 import Section from '@/components/layout/Section'
 import { getActiveNotes } from '@/utilities/data'
 import { useState } from 'react'
+import { useSearchParams } from 'react-router-dom'
 
 const Home = () => {
-  const [keyword, setKeyword] = useState('')
+  const [searchParams, setSearchParams] = useSearchParams()
+  const keyword = searchParams.get('keyword') || ''
   const [notes] = useState(getActiveNotes())
 
   const notesToRender = notes.filter((note) =>
     note.title.toLowerCase().includes(keyword.toLowerCase())
   )
 
+  const onSearchHandler = (value) => {
+    setSearchParams({ keyword: value })
+  }
+
   return (
     <div className="min-h-main">
-      <Search value={keyword} setValue={(value) => setKeyword(value)} />
+      <Search value={keyword} setValue={onSearchHandler} />
       <Section title="Catatan Aktif">
         <NoteList notes={notesToRender} />
       </Section>
