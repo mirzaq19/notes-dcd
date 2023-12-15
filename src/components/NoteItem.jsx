0d847b9c7831d217760aef10d6f854861699fa18
M. Auliya Mirzaq Romdloni <mirzaqarjap@gmail.com>
2023-12-15 20:39:32 +0700

    fix: card group hover

--- src/components/NoteItem.jsx
+++ src/components/NoteItem.jsx
@@ -8,7 +8,7 @@ const NoteItem = ({ className, id, title, body, createdAt, ...rest }) => {
   return (
     <div
       className={clsx(
-        'card gap-2 p-4',
+        'card gap-2 p-4 group',
         'border border-secondary rounded-md shadow-sm',
         className
       )}
@@ -16,9 +16,16 @@ const NoteItem = ({ className, id, title, body, createdAt, ...rest }) => {
     >
       <div className="text-center">
         <h3 className="line-clamp-1 text-ellipsis">
-          <CustomLink href={`/notes/${id}`}>{title}</CustomLink>
+          <CustomLink
+            className="group-hover:border-transparent"
+            href={`/notes/${id}`}
+          >
+            {title}
+          </CustomLink>
         </h3>
-        <p className="text-gray-600">{showFormattedDate(createdAt)}</p>
+        <p className="text-gray-500 text-xs mt-1 tracking-wider md:text-sm">
+          {showFormattedDate(createdAt)}
+        </p>
       </div>
       <hr className="border-0 border-b border-secondary" />
       <p className="line-clamp-6 text-ellipsis">{parse(body)}</p>
