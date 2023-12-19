3ead34cd065bfcbe56cf6f2a98a543394fafd780
M. Auliya Mirzaq Romdloni <mirzaqarjap@gmail.com>
2023-12-19 14:47:11 +0700

    feat: make toggle button more modular

--- src/components/buttons/ThemeButton.jsx
+++ src/components/buttons/ThemeButton.jsx
@@ -1,25 +1,7 @@
 import useTheme from '@/contexts/theme'
-import clsx from 'clsx'
 import { IoSunnyOutline, IoMoonOutline } from 'react-icons/io5'
 import PropTypes from 'prop-types'
-
-const IconContainer = ({ children }) => {
-  return (
-    <div
-      className={clsx(
-        'flex justify-center items-center',
-        'bg-white dark:bg-dark',
-        'text-lg md:text-2xl w-8 h-8 md:w-12 md:h-12'
-      )}
-    >
-      {children}
-    </div>
-  )
-}
-
-IconContainer.propTypes = {
-  children: PropTypes.node.isRequired,
-}
+import ToggleButton from '@/components/buttons/ToggleButton'
 
 const ThemeButton = ({ className, ...rest }) => {
   const { themeMode, lightTheme, darkTheme } = useTheme()
@@ -33,34 +15,14 @@ const ThemeButton = ({ className, ...rest }) => {
   }
 
   return (
-    <button
-      className={clsx(
-        'flex group justify-center items-start overflow-hidden',
-        'bg-gradient-to-br from-accent-peach to-accent-grape',
-        'w-8 h-8 md:w-12 md:h-12',
-        'border border-secondary hover:border-accent-peach hover:text-accent-peach rounded-md',
-        'transition-colors duration-300 ease-in-out',
-        className
-      )}
+    <ToggleButton
+      className={className}
+      firstEl={<IoMoonOutline />}
+      secondEl={<IoSunnyOutline />}
       onClick={onThemeChangeHandler}
+      toggleCallback={() => themeMode === 'dark'}
       {...rest}
-    >
-      <div
-        className={clsx(
-          'transition-transform duration-300 ease-in-out',
-          themeMode === 'dark'
-            ? '-translate-y-8 md:-translate-y-12'
-            : 'translate-x-0'
-        )}
-      >
-        <IconContainer>
-          <IoMoonOutline />
-        </IconContainer>
-        <IconContainer>
-          <IoSunnyOutline />
-        </IconContainer>
-      </div>
-    </button>
+    />
   )
 }
 
