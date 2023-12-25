718f38e5f9fd09cdacedaa650c4831c0891397dc
M. Auliya Mirzaq Romdloni <mirzaqarjap@gmail.com>
2023-12-25 13:20:05 +0700

    feat: add validation to register form

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -4,7 +4,7 @@ import CustomLink from '@/components/links/CustomLink'
 import { register } from '@/utilities/network-data'
 import { useNavigate } from 'react-router-dom'
 import toast from 'react-hot-toast'
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import { AiOutlineLoading3Quarters } from 'react-icons/ai'
 
 const Register = () => {
@@ -12,6 +12,16 @@ const Register = () => {
   const [name, setName] = useState('')
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
+  const nameRef = useRef('')
+  const emailRef = useRef('')
+  const passwordRef = useRef('')
+  const passwordConfirmRef = useRef('')
+  const [errors, setErrors] = useState({
+    name: '',
+    email: '',
+    password: '',
+    passwordConfirm: '',
+  })
   const [passwordConfirm, setPasswordConfirm] = useState('')
   const [registering, setRegistering] = useState(false)
 
@@ -23,6 +33,24 @@ const Register = () => {
 
   const onRegisterSubmitHandler = async (e) => {
     e.preventDefault()
+
+    if (!nameValidationHandler()) {
+      nameRef.current.focus()
+      return
+    }
+    if (!emailValidationHandler()) {
+      emailRef.current.focus()
+      return
+    }
+    if (!passwordValidationHandler()) {
+      passwordRef.current.focus()
+      return
+    }
+
+    if (!passwordConfirmValidationHandler()) {
+      passwordConfirmRef.current.focus()
+      return
+    }
 
     const data = {
       name,
@@ -46,6 +74,80 @@ const Register = () => {
     )
   }
 
+  const nameValidationHandler = () => {
+    if (!name) {
+      setErrors((prev) => ({
+        ...prev,
+        name: 'Name is required',
+      }))
+      return false
+    }
+    setErrors((prev) => ({
+      ...prev,
+      name: '',
+    }))
+    return true
+  }
+
+  const emailValidationHandler = () => {
+    if (!email) {
+      setErrors((prev) => ({
+        ...prev,
+        email: 'Email is required',
+      }))
+      return false
+    }
+    if (email !== '' && !email.includes('@')) {
+      setErrors((prev) => ({
+        ...prev,
+        email: 'Email is invalid',
+      }))
+      return false
+    }
+    setErrors((prev) => ({
+      ...prev,
+      email: '',
+    }))
+    return true
+  }
+
+  const passwordValidationHandler = () => {
+    if (!password) {
+      setErrors((prev) => ({
+        ...prev,
+        password: 'Password is required',
+      }))
+      return false
+    }
+    setErrors((prev) => ({
+      ...prev,
+      password: '',
+    }))
+    return true
+  }
+
+  const passwordConfirmValidationHandler = () => {
+    if (!passwordConfirm) {
+      setErrors((prev) => ({
+        ...prev,
+        passwordConfirm: 'Password confirmation is required',
+      }))
+      return false
+    }
+    if (passwordConfirm !== password) {
+      setErrors((prev) => ({
+        ...prev,
+        passwordConfirm: 'Password confirmation is not match',
+      }))
+      return false
+    }
+    setErrors((prev) => ({
+      ...prev,
+      passwordConfirm: '',
+    }))
+    return true
+  }
+
   return (
     <div className="min-h-main">
       <div className="my-8">
@@ -54,53 +156,84 @@ const Register = () => {
           <p className="tracking-wider">Register to use the app, please</p>
         </div>
         <div className="flex justify-center">
-          <form className="max-w-xl" onSubmit={onRegisterSubmitHandler}>
+          <form className="max-w-xl w-full" onSubmit={onRegisterSubmitHandler}>
             <label className="inline-block mb-2" htmlFor="nama">
               Nama
             </label>
             <Input
               id="nama"
-              className="mb-3"
+              className={
+                errors.name ? 'border-red-600 dark:border-red-400' : ''
+              }
               placeholder="nama"
               value={name}
               setValue={setName}
+              ref={nameRef}
+              onBlur={nameValidationHandler}
               required
             />
+            <p className="mt-2 mb-3 text-sm text-red-600 dark:text-red-400">
+              {errors.name}
+            </p>
             <label className="inline-block mb-2" htmlFor="email">
               Email
             </label>
             <Input
               id="email"
-              className="mb-3"
+              className={
+                errors.email ? 'border-red-600 dark:border-red-400' : ''
+              }
               placeholder="email"
               type="email"
               value={email}
               setValue={setEmail}
+              ref={emailRef}
+              onBlur={emailValidationHandler}
               required
             />
+            <p className="mt-2 mb-3 text-sm text-red-600 dark:text-red-400">
+              {errors.email}
+            </p>
             <label className="inline-block mb-2" htmlFor="password">
               Password
             </label>
             <Input
               id="password"
-              className="mb-3"
+              className={
+                errors.password ? 'border-red-600 dark:border-red-400' : ''
+              }
               placeholder="******"
               type="password"
               value={password}
               setValue={setPassword}
+              ref={passwordRef}
+              onBlur={passwordValidationHandler}
               required
             />
+            <p className="mt-2 mb-3 text-sm text-red-600 dark:text-red-400">
+              {errors.password}
+            </p>
             <label className="inline-block mb-2" htmlFor="passwordConfirm">
               Password Confirmation
             </label>
             <Input
               id="passwordConfirm"
+              className={
+                errors.passwordConfirm
+                  ? 'border-red-600 dark:border-red-400'
+                  : ''
+              }
               placeholder="******"
               type="password"
               value={passwordConfirm}
               setValue={setPasswordConfirm}
+              ref={passwordConfirmRef}
+              onBlur={passwordConfirmValidationHandler}
               required
             />
+            <p className="mt-2 mb-3 text-sm text-red-600 dark:text-red-400">
+              {errors.passwordConfirm}
+            </p>
             <p className="my-4">
               have an account?{' '}
               <CustomLink className="pb-1 dark:text-zinc-300" href="/login">
