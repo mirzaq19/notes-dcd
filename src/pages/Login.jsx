754a0edf0066a9a884ee3c8a32d4eaa406cd3d4c
M. Auliya Mirzaq Romdloni <mirzaqarjap@gmail.com>
2023-12-25 13:13:37 +0700

    feat: add validation to login form

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { AiOutlineLoading3Quarters } from 'react-icons/ai'
 import toast from 'react-hot-toast'
@@ -14,6 +14,12 @@ const Login = () => {
   const [password, setPassword] = useState('')
   const { loginDispatch } = useLogin()
   const navigate = useNavigate()
+  const [errors, setErrors] = useState({
+    email: '',
+    password: '',
+  })
+  const emailRef = useRef('')
+  const passwordRef = useRef('')
 
   const loginHandler = async () => {
     const { error, data } = await login({
@@ -30,8 +36,16 @@ const Login = () => {
   }
 
   const onSubmitHandler = async (e) => {
-    setLogginIn(true)
     e.preventDefault()
+    if (!emailValidationHandler()) {
+      emailRef.current.focus()
+      return
+    }
+    if (!passwordValidationHandler()) {
+      passwordRef.current.focus()
+      return
+    }
+    setLogginIn(true)
 
     toast.promise(
       loginHandler().finally(() => {
@@ -46,6 +60,45 @@ const Login = () => {
     )
   }
 
+  const emailValidationHandler = () => {
+    if (email === '') {
+      setErrors((prev) => ({
+        ...prev,
+        email: 'Email is required',
+      }))
+      return false
+    }
+    if (email !== '' && !email.includes('@')) {
+      setErrors((prev) => ({
+        ...prev,
+        email: 'Email is invalid',
+      }))
+      return false
+    }
+
+    setErrors((prev) => ({
+      ...prev,
+      email: '',
+    }))
+    return true
+  }
+
+  const passwordValidationHandler = () => {
+    if (password === '') {
+      setErrors((prev) => ({
+        ...prev,
+        password: 'Password is required',
+      }))
+      return false
+    }
+
+    setErrors((prev) => ({
+      ...prev,
+      password: '',
+    }))
+    return true
+  }
+
   return (
     <div className="min-h-main">
       <div className="my-8">
@@ -54,30 +107,45 @@ const Login = () => {
           <p className="tracking-wider">Login to use the app, please</p>
         </div>
         <div className="flex justify-center">
-          <form className="max-w-xl" onSubmit={onSubmitHandler}>
+          <form className="max-w-xl w-full" onSubmit={onSubmitHandler}>
             <label className="inline-block mb-2" htmlFor="email">
               Email
             </label>
             <Input
               id="email"
-              className="mb-3"
+              className={
+                errors.email ? 'border-red-600 dark:border-red-400' : ''
+              }
               placeholder="email"
               type="email"
               value={email}
               setValue={setEmail}
+              onBlur={emailValidationHandler}
+              ref={emailRef}
               required
             />
+            <p className="mt-2 mb-3 text-sm text-red-600 dark:text-red-400">
+              {errors.email}
+            </p>
             <label className="inline-block mb-2" htmlFor="password">
               Password
             </label>
             <Input
               id="password"
+              className={
+                errors.password ? 'border-red-600 dark:border-red-400' : ''
+              }
               placeholder="******"
               type="password"
               value={password}
               setValue={setPassword}
+              onBlur={passwordValidationHandler}
+              ref={passwordRef}
               required
             />
+            <p className="mt-2 mb-3 text-sm text-red-600 dark:text-red-400">
+              {errors.password}
+            </p>
 
             <p className="my-4">
               don&apos;t have an account?{' '}
@@ -85,7 +153,6 @@ const Login = () => {
                 register
               </CustomLink>
             </p>
-
             <Button className="w-full md:w-a" disabled={logginIn}>
               {logginIn ? (
                 <AiOutlineLoading3Quarters className="animate-spin" />
