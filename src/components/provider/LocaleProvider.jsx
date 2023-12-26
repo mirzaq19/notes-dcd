482f78775cd02b76b6e77981c6ec21a5758e3ff3
M. Auliya Mirzaq Romdloni <mirzaqarjap@gmail.com>
2023-12-26 15:36:07 +0700

    feat: save locale preference to localstorage

--- src/components/provider/LocaleProvider.jsx
+++ src/components/provider/LocaleProvider.jsx
@@ -2,10 +2,14 @@ import { useState } from 'react'
 import { LocaleContext } from '@/contexts/locale'
 
 const LocaleProvider = ({ children }) => {
-  const [locale, setLocale] = useState('id')
+  const localStorageLocale = localStorage.getItem('locale')
+  const [locale, setLocale] = useState(
+    localStorageLocale ? localStorageLocale : 'id'
+  )
 
   const toggleLocale = () => {
     setLocale((prev) => (prev === 'id' ? 'en' : 'id'))
+    localStorage.setItem('locale', locale === 'id' ? 'en' : 'id')
   }
 
   return (
