cf292cad68c38a4e0c639359a403b209b0e07bba
M. Auliya Mirzaq Romdloni <mirzaqarjap@gmail.com>
2023-12-26 09:46:42 +0700

    feat: integrating add note page with api

--- src/components/form/InputWithCounter.jsx
+++ src/components/form/InputWithCounter.jsx
@@ -1,49 +1,59 @@
-import { useEffect, useState } from 'react'
+import { forwardRef, useEffect, useState } from 'react'
 import Input from '@/components/form/Input'
-import clsx from 'clsx'
 import PropTypes from 'prop-types'
 
-const InputWithCounter = ({
-  className = '',
-  placeholder = '',
-  value,
-  setValue,
-  maxCounter = 50,
-  ...rest
-}) => {
-  const [counter, setCounter] = useState(maxCounter)
+const InputWithCounter = forwardRef(
+  (
+    {
+      className = '',
+      inputClassName = '',
+      placeholder = '',
+      value,
+      setValue,
+      maxCounter = 50,
+      ...rest
+    },
+    ref
+  ) => {
+    const [counter, setCounter] = useState(maxCounter)
 
-  const onChangeHandler = (value) => {
-    if (value.length <= maxCounter) {
-      setCounter(maxCounter - value.length)
-      setValue(value)
+    const onChangeHandler = (value) => {
+      if (value.length <= maxCounter) {
+        setCounter(maxCounter - value.length)
+        setValue(value)
+      }
     }
-  }
 
-  useEffect(() => {
-    setCounter(maxCounter - value.length)
-  }, [maxCounter, value])
+    useEffect(() => {
+      setCounter(maxCounter - value.length)
+    }, [maxCounter, value])
 
-  return (
-    <div className={clsx('mb-4', className)} {...rest}>
-      <div className="text-end mb-1">
-        <span>Sisa karakter: {counter}</span>
+    return (
+      <div className={className}>
+        <div className="text-end mb-1">
+          <span>Sisa karakter: {counter}</span>
+        </div>
+        <Input
+          className={inputClassName}
+          placeholder={placeholder}
+          value={value}
+          setValue={onChangeHandler}
+          ref={ref}
+          {...rest}
+        />
       </div>
-      <Input
-        placeholder={placeholder}
-        value={value}
-        setValue={onChangeHandler}
-      />
-    </div>
-  )
-}
+    )
+  }
+)
 
 InputWithCounter.propTypes = {
   className: PropTypes.string,
+  inputClassName: PropTypes.string,
   placeholder: PropTypes.string,
   value: PropTypes.string.isRequired,
   setValue: PropTypes.func.isRequired,
   maxCounter: PropTypes.number,
 }
 
+InputWithCounter.displayName = 'InputWithCounter'
 export default InputWithCounter
--- src/pages/AddNote.jsx
+++ src/pages/AddNote.jsx
@@ -3,17 +3,26 @@ import Button from '@/components/buttons/Button'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCirclePlus } from '@fortawesome/free-solid-svg-icons'
 import InputWithCounter from '@/components/form/InputWithCounter'
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import EditableDiv from '@/components/form/EditableDiv'
-import { addNote } from '@/utilities/data'
+import { addNote } from '@/utilities/network-data'
 import { useNavigate } from 'react-router-dom'
+import toast from 'react-hot-toast'
 
 const AddNote = () => {
+  const maxTitleLength = 50
   const navigate = useNavigate()
   const [note, setNote] = useState({
     title: '',
     body: '',
   })
+  const [loading, setLoading] = useState(false)
+  const [errors, setErrors] = useState({
+    title: '',
+    body: '',
+  })
+  const titleRef = useRef('')
+  const bodyRef = useRef('')
 
   const onTitleChangeHandler = (value) => {
     setNote((prevState) => ({
@@ -29,11 +38,81 @@ const AddNote = () => {
     }))
   }
 
+  const titleValidationHandler = () => {
+    const value = note.title
+    if (value.length === 0) {
+      setErrors((prevState) => ({
+        ...prevState,
+        title: 'Title is required',
+      }))
+
+      return false
+    }
+
+    if (value.length > maxTitleLength) {
+      setErrors((prevState) => ({
+        ...prevState,
+        title: 'Title is too long',
+      }))
+
+      return false
+    }
+
+    setErrors((prevState) => ({
+      ...prevState,
+      title: '',
+    }))
+    return true
+  }
+
+  const bodyValidationHandler = () => {
+    const value = note.body
+    if (value.length === 0) {
+      setErrors((prevState) => ({
+        ...prevState,
+        body: 'Body is required',
+      }))
+
+      return false
+    }
+
+    setErrors((prevState) => ({
+      ...prevState,
+      body: '',
+    }))
+    return true
+  }
+
   const onSubmitHandler = (e) => {
     e.preventDefault()
+    if (!titleValidationHandler()) {
+      titleRef.current.focus()
+      return
+    }
+    if (!bodyValidationHandler()) {
+      bodyRef.current.focus()
+      return
+    }
+
+    setLoading(true)
+    toast.promise(
+      addNote({
+        title: note.title,
+        body: note.body,
+      }).then(({ error }) => {
+        if (error) throw new Error('Failed to create note')
+      }),
+      {
+        loading: 'Loading',
+        success: 'Note created successfully',
+        error: (err) =>
+          err.message ?? 'Something went wrong, please try again later!',
+      }
+    )
+
     onTitleChangeHandler('')
     onBodyChangeHandler('')
-    addNote(note)
+    setLoading(false)
     navigate('/')
   }
   return (
@@ -41,17 +120,35 @@ const AddNote = () => {
       <Section title="Buat Catatan">
         <form onSubmit={onSubmitHandler}>
           <InputWithCounter
+            className={errors.title ? '' : 'mb-3'}
+            inputClassName={
+              errors.title ? 'border-red-600 dark:border-red-400' : ''
+            }
             placeholder="Tuliskan judul catatan"
-            maxCounter={50}
+            maxCounter={maxTitleLength}
             value={note.title}
             setValue={onTitleChangeHandler}
+            ref={titleRef}
+            onBlur={titleValidationHandler}
+            required
           />
+          <p className="mt-2 mb-3 text-sm text-red-600 dark:text-red-400">
+            {errors.title}
+          </p>
           <EditableDiv
+            className={
+              errors.body ? 'border-red-600 dark:border-red-400' : 'mb-3'
+            }
             placeholder="Tuliskan isi catatan"
             setValue={onBodyChangeHandler}
+            ref={bodyRef}
+            onBlur={bodyValidationHandler}
           />
+          <p className="mt-2 mb-3 text-sm text-red-600 dark:text-red-400">
+            {errors.body}
+          </p>
 
-          <Button className="w-full">
+          <Button className="w-full" disabled={loading}>
             <FontAwesomeIcon icon={faCirclePlus} />
             Buat
           </Button>
